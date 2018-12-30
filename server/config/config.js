c6c061fae7d57c04bd8c9d1a80b0e34713dca02f
Francisco Bernal <pacho@MacBook-Air-de-Francisco.local>
2018-12-29 19:55:18 -0500

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,4 @@ process.env.PORT = process.env.PORT || 3000
 process.env.URLDB = process.env.MONGO_URI
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
 process.env.SEED = process.env.SEED || "este-es-el-seed-de-desarollo"
+process.env.CLIENT_ID = process.env.CLIENT_ID || "1001036984668-4v80ug56ktc71k5487tv73crfpgji6ku.apps.googleusercontent.com"
--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ require("./config/config")
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const path = require("path")
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(express.static(path.resolve(__dirname, "../public")))
 
 app.use(require("./routes"))
 
