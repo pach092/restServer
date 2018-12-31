8d3fa59dd4a5a5496601c421f81f68997a8fe88e
Francisco Bernal <pacho@MacBook-Air-de-Francisco.local>
2018-12-30 21:25:02 -0500

    subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -33,7 +33,26 @@ let verificaAdminRole = (req, res, next) => {
     next()
 }
 
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: "Token no valido"
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario
+        next()
+    })
+}
+
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
